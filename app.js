3c908bbb153f9cf674279725d769c01b0d146be8
Juan <juancattaneo92@gmail.com>
2021-04-08 11:55:44 -0700

    routes api questions created

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 const mongoose = require ('mongoose');
 
 const db = require ('./config/keys').mongoURI
+const questions = require('./routes/api/questions')
 
 mongoose
     .connect(db, { userNewUrlParser: true, userUnifiedTopology: true })
     .then( () => console.log("Mongo is working!"))
     .catch(err => console.log(err))
-
 
 
 app.get('/', (req, res) => res.send("this works!"))
