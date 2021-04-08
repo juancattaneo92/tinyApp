7f7ba3cea5eb0a97d056df160dbf29ce6d919c6f
Juan <juancattaneo92@gmail.com>
2021-04-08 11:45:41 -0700

    tiny app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const mongoose = require ('mongoose');
+
+const db = require ('./config/keys').mongoURI
+
+mongoose
+    .connect(db, { userNewUrlParser: true, userUnifiedTopology: true })
+    .then( () => console.log("Mongo is working!"))
+    .catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => res.send("this works!"))
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server is serving on port ${port}`))
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://TinyUser:1vF1YEe8X9tHCzod@cluster0.y2h6e.mongodb.net/test'
+}
